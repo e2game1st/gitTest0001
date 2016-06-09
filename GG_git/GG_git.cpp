774021f505e92870176913b97c21c638e83df347
dlsnzlyun <dlsnzlyun@gmail.com>
2016-06-09 14:49:56 +0900

    qwer

---
+++ GG_git/GG_git.cpp
@@ -0,0 +1,13 @@
+// GG_git.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int main()
+{
+	int a;
+    return 0;
+}
+
---
+++ git_Test/git_Test.cpp
@@ -0,0 +1,19 @@
+// git_Test.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+
+class cTest
+{
+	int a;
+};
+class A : public cTest
+{
+
+};
+
+int main()
+{
+    return 0;
+}
+
